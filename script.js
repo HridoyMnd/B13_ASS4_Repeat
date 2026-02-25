947f46fdb8c0289ccd917fa6602eeacef9e4f5c1
Hridoy Munda <hridoymnd10@gmail.com>
2026-02-25 17:20:55 +0600

    added dynamically application card show

--- script.js
+++ script.js
@@ -5,19 +5,32 @@ function tabSwitch(tab) {
     const interviewTab = document.getElementById("interview_tab");
     const rejectedTab = document.getElementById("rejected_tab");
     const tabs = document.getElementById("tabs_container").children;
-    for(const tab of tabs){
+    const allCardContainer = document.getElementById("all_card_container")
+    const interviewCardContainer = document.getElementById("interview_card_container")
+    const rejectedCardContainer = document.getElementById("rejected_card_container")
+
+    for (const tab of tabs) {
         tab.classList.remove("bg-blue-500", "text-white")
-        tab.classList.add("bg-white", "shadow-sm")  
+        tab.classList.add("bg-white", "shadow-sm")
     }
-    
+
     if (tab === "all_tab") {
         allTab.classList.remove("bg-white", "shadow-sm")
         allTab.classList.add("bg-blue-500", "text-white")
+        rejectedCardContainer.classList.add("hidden")
+        interviewCardContainer.classList.add("hidden")
+        allCardContainer.classList.remove("hidden")
     } else if (tab === "interview_tab") {
         interviewTab.classList.remove("bg-white", "shadow-sm")
         interviewTab.classList.add("bg-blue-500", "text-white")
+        rejectedCardContainer.classList.add("hidden");
+        allCardContainer.classList.add("hidden");
+        interviewCardContainer.classList.remove("hidden");
     } else {
         rejectedTab.classList.remove("bg-white", "shadow-sm")
         rejectedTab.classList.add("bg-blue-500", "text-white")
+        allCardContainer.classList.add("hidden");
+        interviewCardContainer.classList.add("hidden");
+        rejectedCardContainer.classList.remove("hidden");
     }
 }
